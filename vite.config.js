ec74e1ae6a384363c1ce8b17c64cb1bbc7e304a0
DeepakErothi <you@example.com>
2026-04-08 17:36:20 +0530

    Fix 404 - use absolute paths with base /MAHIMA-DENTAL/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/MAHIMA-DENTAL/',
+  build: {
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
